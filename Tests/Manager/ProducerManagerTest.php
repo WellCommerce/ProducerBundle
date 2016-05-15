06fa49eb547b5075edfc0096d2bb8167868f5ae9
Adam Piotrowski <adam@wellcommerce.org>
2016-05-15 18:52:05 +0200

    Fixed manager tests

---
+++ Tests/Manager/ProducerManagerTest.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * WellCommerce Open-Source E-Commerce Platform
+ * 
+ * This file is part of the WellCommerce package.
+ *
+ * (c) Adam Piotrowski <adam@wellcommerce.org>
+ * 
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace WellCommerce\Bundle\ProducerBundle\Tests\Manager;
+
+use WellCommerce\Bundle\CoreBundle\Test\Manager\AbstractManagerTestCase;
+
+/**
+ * Class ProducerManagerTest
+ *
+ * @author  Adam Piotrowski <adam@wellcommerce.org>
+ */
+class ProducerManagerTest extends AbstractManagerTestCase
+{
+    protected function get()
+    {
+        return $this->container->get('producer.manager');
+    }
+}
